4dff45c4c050da383bc8dc11983fd608392e456c
moskowitza <moskowitzaaron@mac.com>
2018-06-18 17:30:04 -0400

    fixing selectors

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -21,25 +21,25 @@ $("#scrape").on("click", function () {
 $(".savenote").on("click", function () {
   // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
-
+  console.log("thisID : "+thisId)
   // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
     method: "POST",
     url: "/articles/" + thisId,
     data: {
       // Value taken from title input
-      title: $(".titleinput").val(),
+      title: $(".titleinput_"+thisId).val(),
       // Value taken from note textarea
-      body: $(".bodyinput").val()
+      body: $(".bodyinput_"+thisId).val()
     }
   })
     // With that done
     .then(function (data) {
       // Log the response
-      console.log(data);
+      console.log("saved response Data "+data);
       // Remove the values entered in the input and textarea for note entry
-      $(".titleinput").val("");
-      $(".bodyinput").val("");
+      $(".titleinput_"+thisId).val("");
+      $(".bodyinput_"+thisId).val("");
       location.reload();
     });
 
