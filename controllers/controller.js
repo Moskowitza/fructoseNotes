777d835a8b789c82078844e22869d2eb40cd51d5
moskowitza <moskowitzaaron@mac.com>
2018-06-19 09:49:55 -0400

    heroku ready

--- controllers/controller.js
+++ controllers/controller.js
@@ -12,7 +12,13 @@ var db = require("../models");
 var router = express.Router();
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/hiFructose");
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/hiFructose";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 //0) OUR SCRAPE : THIS WORKS: Does 
 router.get("/scrape", function (req, res) {
